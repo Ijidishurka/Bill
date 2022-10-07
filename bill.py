99b2c0712ffe9c9439b0cf29bbcbd95b0d517510
Ijidishurka <108403538+Ijidishurka@users.noreply.github.com>
2022-10-07 08:26:51 +0300

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,23 @@
+# отпровляет видео сообщение с Биллом Херрингтон
+# канал @modwini
+from .. import loader
+
+
+@loader.tds
+class billy(loader.Module):
+    """Подпишись на канал @modwini"""
+
+    strings = {"name": "Billy"}
+
+    async def billycmd(self, message):
+        """Отправляет видео сообщение"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://telesco.pe/kruglishik/31",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
